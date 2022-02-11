9c430e051304554f5df3361ba5f234cd8e6d1507
rodennis <rev.rltodd@gmail.com>
2022-02-11 10:28:34 -0600

    run npm run build

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -9,7 +9,7 @@ const getToken = () => {
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
         ? 'https://hcf-reviews-backend.herokuapp.com/api'
-        : 'http://localhost:3000/api'
+        : 'https://hcf-reviews-backend.herokuapp.com/api'
 })
 
 api.interceptors.request.use(async function (config) {
